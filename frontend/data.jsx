e80c1313c713646dea93c6a94019748ca8fcfe6f
bascom241 <abdulbasitabdulwahab21@gmail.com>
2024-10-11 18:37:01 +0100

    first commit
    This is a full stack ecommerce

---
+++ frontend/data.jsx
@@ -0,0 +1,25 @@
+import image1 from './src/assets/R1.png'
+import image2 from './src/assets/R2.jpg'
+import image3 from './src/assets/Image2.jpg'
+
+const products = [
+    {
+        id:1,
+        image: image1,
+        name:'Iphones',
+    },
+    {
+        id:2, 
+        image: image2,
+        name:'Samsung'
+    },
+    {
+        id:3,
+        image:image3,
+        name:'Earpods'
+    }
+   
+]
+
+
+export default products
